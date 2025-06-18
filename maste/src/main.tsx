95690c4e4aca5580181d906f42a67a03419da806
MaccedoFilho <ricardomacedor@icloud.com>
2025-06-18 01:16:37 -0300

    Feat: removendo código padrão e adicionando gsap

--- maste/src/main.tsx
+++ maste/src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { gsap } from "gsap";
+
+import { CustomEase } from "gsap/CustomEase";
+    
+import { Flip } from "gsap/Flip";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(Flip,MotionPathPlugin,ScrollTrigger,ScrollSmoother,SplitText,CustomEase);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
